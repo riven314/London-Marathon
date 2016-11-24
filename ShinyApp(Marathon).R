5bb1967d781a664e3fad14cddaa554c5e74623f2
Alex <u3509807@hku.hk>
2016-11-24 09:02:45 +0800

    Add files via upload

--- ShinyApp(Marathon).R
+++ ShinyApp(Marathon).R
@@ -19,7 +19,7 @@ library(ggplot2)
 library(reshape2)
 library(plotly)
 
-## **All the time are in timezone Hongkong and the date is "2016-11-17"
+# **All the time are in timezone Hongkong and the date is "2016-11-17"
 finalized_POSIX <- read.csv("finalized_data.csv")
 name_list <- c("RunnerNo","Name","Gender","Category","PlaceOverall","PlaceGender","PlaceCategory","5K","10K","15K","20K","21K","25K","30K","35K","40K","42K")
 names(finalized_POSIX) <- name_list
@@ -52,7 +52,10 @@ ui <- dashboardPage(
                          tabName = "ready",badgeColor = "green"),
                 menuItem("Boxplot", icon = icon("users"), 
                         tabName = "box",badgeColor = "red"),
-                
+                menuItem("Speed vs Distance",icon=icon("flash",lib="glyphicon"),
+                        tabName="speed",badgeColor="yellow"),
+                menuItem("Speed vs Ranking",icon=icon("sort",lib="glyphicon"),
+                         tabName="ani_speed",badgeColor = "purple"),
                 menuItem("Ranking vs Time", icon = icon("glyphicon glyphicon-signal",lib="glyphicon"),
                         tabName = "ranking",badgeColor = "blue"),
                 
@@ -66,6 +69,38 @@ dashboardBody(
   
   
   tabItems(
+    
+    tabItem(
+      tabName = "speed",
+      fluidPage(
+        titlePanel(
+
+          selectInput("graph",h4("Plot Type"),
+                      choices=c("Boxplot","Line"))
+        ),
+        mainPanel(
+          plotOutput("plot4")
+        )
+      )),   
+    
+    tabItem(
+      tabName = "ani_speed",
+      fluidPage(
+        titlePanel(
+        sliderInput("km",h4("Select a Phrase (Km)"),
+                    min=5,
+                    max=42,step=5,
+                    value=c(5),ticks=FALSE,animate=animationOptions(interval=2300,loop=TRUE))
+
+        ),
+        mainPanel(
+          plotOutput("plot5")
+        )
+      )),   
+    
+    
+    
+    
     
     tabItem(
       tabName= "ready",
@@ -333,6 +368,128 @@ server <- function(input,output)  {
         ggtitle("Scatter Plot on Ranking vs Time")
     },width=850,height=500)
     
+    
+  ############### NEW ###########
+    
+    mile_names <- c("RunnerNo","Name","Gender","Category","PlaceOverall","PlaceGender","PlaceCategory","K5","K10","K15","K20","K21","K25","K30","K35","K40","K42")
+    finalized_POSIX2 <- finalized_POSIX
+    names(finalized_POSIX2) <- mile_names
+    
+    
+    finalized_POSIX2 = mutate(finalized_POSIX2, 
+                              Speed42_40=1000*2/as.numeric(difftime(K42,K40,units="mins")),
+                              Speed40_35=1000*5/as.numeric(difftime(K40,K35,units="mins")),
+                              Speed35_30=1000*5/as.numeric(difftime(K35,K30,units="mins")),
+                              Speed30_25=1000*5/as.numeric(difftime(K30,K25,units="mins")),
+                              Speed25_21=1000*4/as.numeric(difftime(K25,K21,units="mins")),
+                              Speed21_20=1000/as.numeric(difftime(K21,K20,units="mins")),
+                              Speed20_15=1000*5/as.numeric(difftime(K20,K15,units="mins")),
+                              Speed15_10=1000*5/as.numeric(difftime(K15,K10,units="mins")),
+                              Speed10_5=1000*5/as.numeric(difftime(K10,K5,units="mins")),
+                              Speed5_0=1000*5/as.numeric(difftime(K5,as.POSIXct("2016-11-17 00:00:00","Hongkong"),units="mins"))
+    )
+    finalized_POSIX2 <- na.omit(finalized_POSIX2)
+    
+    finalized_POSIX2$OverallRanking <- vector(mode="numeric",length = nrow(finalized_POSIX2))
+    for (i in 1:nrow(finalized_POSIX2)) {
+      if (0<= finalized_POSIX2$PlaceOverall[i]&finalized_POSIX2$PlaceOverall[i]<=10000) {finalized_POSIX2$OverallRanking[i]=1}
+      else if (10000<finalized_POSIX2$PlaceOverall[i]&finalized_POSIX2$PlaceOverall[i]<=20000) {finalized_POSIX2$OverallRanking[i]=2}
+      else if (20000<finalized_POSIX2$PlaceOverall[i]&finalized_POSIX2$PlaceOverall[i]<=30000) {finalized_POSIX2$OverallRanking[i]=3}
+      else if (30000<finalized_POSIX2$PlaceOverall[i]&finalized_POSIX2$PlaceOverall[i]<=40000) {finalized_POSIX2$OverallRanking[i]=4}}
+    finalized_POSIX2$OverallRanking <- as.factor(finalized_POSIX2$OverallRanking)
+    
+    
+    Distance <- c(5,10,15,20,21,25,30,35,40,42)
+    Ranking1 <- c(mean(filter(finalized_POSIX2,OverallRanking==1)[,27]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,26]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,25]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,24]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,23]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,22]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,21]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,20]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,19]),
+                  mean(filter(finalized_POSIX2,OverallRanking==1)[,18])
+    )
+    Ranking2 <- c(mean(filter(finalized_POSIX2,OverallRanking==2)[,27]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,26]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,25]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,24]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,23]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,22]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,21]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,20]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,19]),
+                  mean(filter(finalized_POSIX2,OverallRanking==2)[,18])
+    )
+    
+    Ranking3 <- c(mean(filter(finalized_POSIX2,OverallRanking==3)[,27]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,26]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,25]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,24]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,23]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,22]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,21]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,20]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,19]),
+                  mean(filter(finalized_POSIX2,OverallRanking==3)[,18])
+    )
+    
+    Ranking4 <- c(mean(filter(finalized_POSIX2,OverallRanking==4)[,27]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,26]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,25]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,24]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,23]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,22]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,21]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,20]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,19]),
+                  mean(filter(finalized_POSIX2,OverallRanking==4)[,18])
+    )
+    
+    mean_time <- data.frame(Distance,Ranking1,Ranking2,Ranking3,Ranking4)
+    
+    mean_time <- melt(mean_time,
+                      id.vars = c("Distance"))
+    
+    finalized_POSIX3 <- melt(finalized_POSIX2,
+                             id.vars = c("RunnerNo","Name","Gender","Category","PlaceOverall","PlaceGender","PlaceCategory","K5", "K10","K15","K20","K21","K25","K30","K35","K40","K42","OverallRanking"), 
+                             measure.vars = c("Speed42_40", "Speed40_35","Speed35_30","Speed30_25","Speed25_21","Speed21_20","Speed20_15","Speed15_10","Speed10_5","Speed5_0"))
+    finalized_POSIX3$variable <- sapply(as.character(finalized_POSIX3$variable), switch, 
+                                        "Speed42_40"=42,"Speed40_35"=40, "Speed35_30"=35, "Speed30_25"=30, "Speed25_21"=25,"Speed21_20"=21,
+                                        "Speed20_15"=20,"Speed15_10"=15,"Speed10_5"=10,"Speed5_0"=5,USE.NAMES = F)
+    finalized_POSIX3[,19] <- as.factor(finalized_POSIX3[,19])
+    
+    output$plot4 <- renderPlot({
+      if (input$graph=="Boxplot") 
+            {ggplot(finalized_POSIX3,aes(x=variable,y=value))+
+          geom_boxplot(aes(color=OverallRanking)) + ggtitle("Speed Distribution in Different Phrases")+labs(y="Speed (Metres/Min)",x="From 0Km to 42Km")+
+          theme(legend.position="bottom") + scale_y_continuous(limits = c(0, 300))
+        }
+      else if (input$graph=="Line")
+            {ggplot(mean_time,aes(x=Distance,y=value)) + 
+          geom_line(size=0.9,aes(color=mean_time$variable)) +
+          geom_point(size=3,color="gray15")+labs(x="Distance (Km)", y="Average Speed (Metres/Min)") +
+          ggtitle("Average Speed in Different Phrase") + theme(legend.position="none")}
+    },width=1000)
+    
+    km <- reactive({
+      if (input$km == 5)    {27}
+      else if (input$km == 10) {26}
+      else if (input$km == 15) {25}
+      else if (input$km == 20) {24}
+      else if (input$km == 25) {22}
+      else if (input$km == 30) {21}
+      else if (input$km == 35) {20}
+      else if (input$km == 40) {19}
+      else if (input$km == 42) {18}
+    })
+    
+    output$plot5 <- renderPlot({
+      ggplot(finalized_POSIX2,aes(x=PlaceOverall,y=finalized_POSIX2[,km()],color=Gender))+geom_point()+
+        scale_y_continuous(limits = c(0, 400))+labs(x="Overall Ranking",y="Speed (Metres/Min)") + ggtitle(paste("Speed on",input$km,"Km",sep=" "))
+    },width=1000,height=400)
+
 }
 
 # updateSliderInput(session,"date_range",min = (min_max_time[2,]),
